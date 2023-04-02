75ef62ddcbb50089627ac05c4ec9ccd80156cd37
Carolina Giordano <carolinaangiordano@gmail.com>
2023-04-02 03:51:56 -0300

    Ativ 3

---
+++ ExemploWhile.java
@@ -0,0 +1,17 @@
+public class ExemploWhile {
+    public static void main(String[] args) {
+        int valor = 5;
+        int numero = 0;
+        while (numero != 10) {
+            // enquanto a variável não for 10, o código será repetido
+            System.out.println("Número informado: " + valor);
+            numero = valor;
+            if (numero == 10) {
+                System.out.println("Você acertou!");
+            } else {
+                System.out.println("Você errou :(");
+            }
+            valor++;
+        }
+    }
+}
---
+++ RadarDeTransito.java
@@ -0,0 +1,21 @@
+public class RadarDeTransito {
+    public static void main(String[] args) {
+
+        double velocidadeVeiculo = 60;
+        double velocidadeMaxima = 70;
+
+        double velocidadeMaxPermitida = velocidadeMaxima + (velocidadeMaxima * 0.1);
+
+        if (velocidadeVeiculo > velocidadeMaxPermitida) {
+            System.out.println("Voce será multado");
+        } else {
+            if (velocidadeVeiculo > velocidadeMaxima){
+                System.out.println("Voce está quase atingindo a velocidade maxima");
+            }else {
+                System.out.println("Voce está dentro do limite de velocidade!");
+            }
+        }
+    }
+
+
+}
